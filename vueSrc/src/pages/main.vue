32b80ce6b018fc9abf49ff9953c6ad06ac2228a1
Majitoo <jimmyy5121@gmail.com>
2019-09-08 23:01:13 +0800

    test5

--- vueSrc/src/pages/main.vue
+++ vueSrc/src/pages/main.vue
@@ -280,7 +280,7 @@ export default {
     window.onresize=null;
   },
   mounted() {
-    console.log("test4");
+    console.log("test5");
     this.ez=new ezSnow("body","../../assets/image/snow2.png",30,20)
     .init();
 
